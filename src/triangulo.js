b53d82d60e26bed728c966a3148487c47514b0a7
Igor Moura <imp2@cin.ufpe.br>
2019-10-23 21:55:35 -0300

    feat: finish task 1
    i hate js

--- src/triangulo.js
+++ src/triangulo.js
@@ -1,13 +1,24 @@
-function Triangulo(a,b,c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-  }
+function Triangulo(a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+}
 
-  
-  Triangulo.prototype.toString = function () {
-    return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`
-  };
-  
-  
-  module.exports = Triangulo;
+Triangulo.prototype.tipo = function() {
+  let res = "";
+  if (this.a == this.b && this.b == this.c) res = "Equilatero";
+  else if (this.a != this.b && this.b != this.c && this.c != this.a)
+    res = "Escaleno";
+  else res = "Isosceles";
+  return res;
+};
+
+Triangulo.prototype.valido = function() {
+  return !(
+    this.a + this.b <= this.c ||
+    this.a + this.c <= this.b ||
+    this.b + this.c <= this.a
+  );
+};
+
+module.exports = Triangulo;
